58d9f88001439fb5c87abaf892162f0ba9178ba8
Nikhil483 <53818269+Nikhil483@users.noreply.github.com>
2019-08-07 11:56:02 +0530

    Add files via upload

---
+++ max_element.py
@@ -0,0 +1,18 @@
+list=[]
+max=0
+
+def take_input():
+    n= int(input("enter the number of elements"))
+    for i in range (0,n) :
+        a=int(input("enter the element "+str(i)))
+        list.append(a)
+    print(list)
+
+
+def calculate_max():
+    for i in range (0,n) :
+        if list[i]>max :
+            max=list[i]
+    print("the maximum 
+
+take_input()
