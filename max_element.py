6ccba5e86f95b7ac5ecbf355eef1ee6c7f1808a2
Nikhil483 <53818269+Nikhil483@users.noreply.github.com>
2019-08-07 12:43:24 +0530

    Add files via upload

--- max_element.py
+++ max_element.py
@@ -1,18 +1,28 @@
-list=[]
-max=0
+class maximum:
+    def __init__(self, test_list,maxi,n):
+       self.test_list=test_list
+       self.maxi=maxi
+       self.n=n
 
-def take_input():
-    n= int(input("enter the number of elements"))
-    for i in range (0,n) :
-        a=int(input("enter the element "+str(i)))
-        list.append(a)
-    print(list)
+       
+    def take_input(self):
+        
+        for i in range (0,self.n) :
+            a=int(input("enter the element "+str(i)+" :"))
+            self.test_list.append(a)
+        print(self.test_list)
 
 
-def calculate_max():
-    for i in range (0,n) :
-        if list[i]>max :
-            max=list[i]
-    print("the maximum 
+    def calculate_max(self):
+        for i in range (0,self.n) :
+            if self.test_list[i]>self.maxi :
+                self.maxi=self.test_list[i]
+        print("the maximum value in the list is "+str(self.maxi))
 
-take_input()
+        
+        
+list1 = maximum([],0,0)
+list1.n= int(input("enter the number of elements : "))
+print(list1.n)
+list1.take_input()
+list1.calculate_max()
