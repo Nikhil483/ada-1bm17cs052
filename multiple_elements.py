9fe4bcf4d488b95c07204c63ff0392a061c3ab9c
Nikhil483 <53818269+Nikhil483@users.noreply.github.com>
2019-08-16 14:25:14 +0530

    Add files via upload

--- multiple_elements.py
+++ multiple_elements.py
@@ -1,10 +1,8 @@
 with open('test1.txt') as f:
-        print("hello")
         test_cases=f.readlines()
 
 for i in range(len(test_cases)) :
         test_cases[i]=test_cases[i].rstrip("\n").strip(). split(" ")
-        print(test_cases)
 n=0
 
 no_of_test_cases=len(test_cases)//3
@@ -20,16 +18,18 @@ for i in range(n,len(test_cases),3) :
         while first<=last :
             mid=(first+last)//2
             if key==test_list[mid] :
-                           for i in range(1,mid) :
-                                done=0
-                                if key==test_list[mid-i] :
+                           for i in range(1,mid):
+                              done=0
+                              if key==test_list[mid-i] :
                                     start_index=start_index+1
                                     done=1
-                                if mid+i < len(test_list) and key==test_list[mid+i]:
+                              if key==test_list[mid+i] and mid+i < len(test_list):
                                     last_index=last_index+1
                                     done=1
-                                if done==0 :
-                                    break
+                              if done==0: 
+                                print("breaking out of loop")
+                                break
+                                
             elif test_list[mid]<key :
                 first=mid+1
             else :
@@ -37,4 +37,3 @@ for i in range(n,len(test_cases),3) :
         print("start_index : ",mid-start_index)	
         print("last_index : ",mid+last_index)
         print("count :" ,start_index+last_index+1)
-	#break
